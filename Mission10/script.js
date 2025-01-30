f66cd7861919b08a6b57c60d0e5d17b1c80c0878
Raaaphhh <dcpraphael@gmail.com>
2025-01-30 10:40:55 +0100

    UpdateScript

--- Mission10/script.js
+++ Mission10/script.js
@@ -12,10 +12,37 @@ function AjoutLigne() {
 }
 
 function Calculer(){
-    var listePrix = document.getElementsBy("unitPrice");
-    var listeQte = document.getElementsBy("quantity");
-    var listeTtl = document.getElementsBy("total");
-    for (var i = 0; i < listePrix.length; i++){
-        listeTtl[i].value = listePrix[i].value * listeQte[i].value;
+    var table = document.getElementById("table");
+    
+
+    for (var i = 0, row; row = table.rows[i]; i++) {
+        var totalQuantite = 0;
+        var totalPrixUnitaire = 0;
+        var totalSum = 0;
+        var quantite = parseFloat(row.cells[1].querySelector("input").value) || 0;
+        var unitPrice = parseFloat(row.cells[2].querySelector("input").value) || 0;
+
+        totalQuantite = totalQuantite + quantite;
+        totalPrixUnitaire = totalPrixUnitaire + unitPrice;
+        totalSum = totalSum + quantite * unitPrice;
+
+        row.cells[3].querySelector("input").value = totalSum;
+
+        console.log("Total Quantity: " + totalQuantite);
+        console.log("Total Unit Price: " + totalPrixUnitaire);
     }
 }
+
+// function RemplirAuto(){
+//     let lignes = document.querySelectorAll("tr");
+//     lignes.forEach(function(ligne){
+//         let colonnes = ligne.querySelectorAll("td");
+//         colonnes.forEach(function(colonne){
+//             let input = colonne.querySelector("input");
+//             if(input){
+//                 input.value = "test";
+//             }
+//         });
+//     });
+// }
+
