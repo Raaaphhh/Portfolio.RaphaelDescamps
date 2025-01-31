5890b50bdcdc044c098bd9f1aeaa1ce3f50fe26a
Raaaphhh <dcpraphael@gmail.com>
2025-01-31 21:13:31 +0100

    UpdateMission10

--- Mission10/script.js
+++ Mission10/script.js
@@ -13,14 +13,20 @@ function AjoutLigne() {
 
 function Calculer(){
     var table = document.getElementById("table");
+    
+    var remise = 10; // Remise de 10%
+    var taux_imposition = 10; // imposition de 10%
+    var frais_expedition = 15; // frais d'expedition de 15euro
+
     var sous_total = 0;
     var sous_total_moins_remise = 0;
+    var taxe_totale = 0;
+    var solde = 0;
 
     for (var i = 0, row; row = table.rows[i]; i++) {
         var totalQuantite = 0;
         var totalPrixUnitaire = 0;
         var totalSum = 0;
-        var remise = 1; // Remise de 10%
         var quantite = parseFloat(row.cells[1].querySelector("input").value) || 0;
         var unitPrice = parseFloat(row.cells[2].querySelector("input").value) || 0;
 
@@ -28,28 +34,21 @@ function Calculer(){
         totalPrixUnitaire = totalPrixUnitaire + unitPrice;
         totalSum = totalSum + quantite * unitPrice;
         sous_total = sous_total + totalSum;  // Somme total des dernieres colonnes
-        sous_total_moins_remise = sous_total-(sous_total * remise);
+        taxe_totale = sous_total * taux_imposition / 100; // Taxe totale 
 
         row.cells[3].querySelector("input").value = totalSum;
 
         document.getElementById("sous_total").value = sous_total;
-        document.getElementById("sous_total_moin_remise").value = sous_total_moins_remise;
+
+        document.getElementById("taxetotal").value = taxe_totale;
 
         // console.log("Total Quantity: " + totalQuantite);
         // console.log("Total Unit Price: " + totalPrixUnitaire);   controle sur la console 
     }
+
+    sous_total_moins_remise += sous_total - (sous_total * remise / 100);
+    solde = sous_total_moins_remise + taxe_totale + frais_expedition; // Solde total
+    document.getElementById("sous_total_moin_remise").value = sous_total_moins_remise;
+    document.getElementById("solde").value = solde;
+
 }
-
-// function RemplirAuto(){
-//     let lignes = document.querySelectorAll("tr");
-//     lignes.forEach(function(ligne){
-//         let colonnes = ligne.querySelectorAll("td");
-//         colonnes.forEach(function(colonne){
-//             let input = colonne.querySelector("input");
-//             if(input){
-//                 input.value = "test";
-//             }
-//         });
-//     });
-// }
-
