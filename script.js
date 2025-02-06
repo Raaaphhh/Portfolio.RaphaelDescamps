1ebf16deaf34d1b0d0af856f9d31bfe4c8373604
Raaaphhh <dcpraphael@gmail.com>
2025-02-06 08:40:05 +0100

    soundoffForthemoment

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-//lancement du son
-document.getElementById("btndemare").addEventListener("click", function() {
-    let sound = new Audio("http://127.0.0.1:5501/AssetsHomePage/launchsound.mp3");
-    sound.play(); // Joue immédiatement
+// //lancement du son
+// document.getElementById("btndemare").addEventListener("click", function() {
+//     let sound = new Audio("http://127.0.0.1:5501/AssetsHomePage/launchsound.mp3");
+//     sound.play(); // Joue immédiatement
 
-    sessionStorage.setItem("playSound", "true"); // Indique qu'on joue le son
-    sessionStorage.setItem("audioTimestamp", sound.currentTime); // Sauvegarde la position du son
+//     sessionStorage.setItem("playSound", "true"); // Indique qu'on joue le son
+//     sessionStorage.setItem("audioTimestamp", sound.currentTime); // Sauvegarde la position du son
 
-    setTimeout(() => {
-        window.location.href = "DossMenuPortfolio/MenuHomePage/index.html"; // Redirection après 1 seconde
-    }, 1000);
-});
+//     setTimeout(() => {
+//         window.location.href = "DossMenuPortfolio/MenuHomePage/index.html"; // Redirection après 1 seconde
+//     }, 1000);
+// });
